25877e1b98abd4e7bc86569267d0ec0667a84af5
arfannaqi <muhammadarfannaqi@smkwikrama.sch.id>
2024-08-30 08:38:57 +0700

    revisi2

--- 3.php
+++ 3.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bilangan Pembagi 90</title>
-</head>
-<body>
-    <h1>Bilangan yang Membagi Habis 90 (1-30)</h1>
-    <ul>
-        <?php
-        $bilangan = 90;
-        for ($i = 1; $i <= 30; $i++) {
-            if ($bilangan % $i == 0) {
-                $hasil = $bilangan / $i;
-                echo "<li>$bilangan : $i = $hasil</li>";
-            }
-        }
-        ?>
-    </ul>
-</body>
-</html>
+<?php
+$a = 30;
+$b = 90;
+for ($i = 1; $i <= 30; $i++) {
+    if ($b % $i === 0) {
+        $c = $b / $i;
+        echo "$b : $i = $c<br> ";
+    }
+}
+?>
