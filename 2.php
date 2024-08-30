44c1129d59736ff35c96d2d4f2879e5badcbd235
arfannaqi <muhammadarfannaqi@smkwikrama.sch.id>
2024-08-30 08:31:20 +0700

    revisi

--- 2.php
+++ 2.php
@@ -16,10 +16,10 @@ if ($totalPembelian > 100000) {
     $diskon = 0.07;
 }
 
-// Hitung total pembayaran setelah diskon
+
 $totalSetelahDiskon = $totalPembelian - ($totalPembelian * $diskon);
 
-// Tampilkan hasil
+
 echo "Total pembelian: " . number_format($totalPembelian, 0, ',', '.') . " IDR<br>";
 echo "Diskon: " . ($diskon * 100) . "%<br>";
 echo "Total yang harus dibayar: " . number_format($totalSetelahDiskon, 0, ',', '.') . " IDR";
